8b43a164bc90a8adc3afaadbbacdfbd589d0e400
Mykhailo Hnatyshyn <mykhailo_hnatyshyn@epuakyiw4406.kyiv.epam.com>
2020-02-18 14:49:05 +0200

    project set up

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
+
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3002;
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const options = new DocumentBuilder()
+    .setTitle('Self-service application')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+  await app.listen(port, '0.0.0.0');
+}
+bootstrap();
