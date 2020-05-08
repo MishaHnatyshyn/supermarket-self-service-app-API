92bd7cb38e923d38287ac953b5a6adcc4a24c68b
MishaHnatyshyn <gnatyshynmisha@gmail.com>
2020-05-08 15:39:29 +0300

    update basket line item interface

--- src/modules/basket/interfaces/line-item-details.interface.ts
+++ src/modules/basket/interfaces/line-item-details.interface.ts
@@ -2,6 +2,8 @@ export default interface LineItemDetails {
   id: number;
   product: number;
   name: string;
+  description: string;
+  photo: string;
   price: number;
   quantity: number;
   sum: number;
