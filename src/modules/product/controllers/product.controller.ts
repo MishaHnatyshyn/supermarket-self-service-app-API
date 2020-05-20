dd8a1d6e3d8fab13ac4baa7a5234ec74c7bef556
MishaHnatyshyn <gnatyshynmisha@gmail.com>
2020-05-20 22:55:23 +0300

    fix swagger return entity

--- src/modules/product/controllers/product.controller.ts
+++ src/modules/product/controllers/product.controller.ts
@@ -9,6 +9,7 @@ import SearchQueryParamsPipe from '../../../shared/search-query-params.pipe';
 import SearchQueryDto from '../dto/search-query.dto';
 import ProductSearchDto from '../dto/product-search.dto';
 import ProductBarcodeDto from '../dto/product-barcode.dto';
+import ProductShortDto from "../dto/product-short.dto";
 
 @ApiUseTags('products')
 @Controller('products')
@@ -21,9 +22,9 @@ export default class ProductController {
     return this.productService.getProduct(id);
   }
 
-  @ApiOkResponse({ type: ProductDto })
+  @ApiOkResponse({ type: ProductShortDto })
   @Get('barcode/:barcode')
-  getProductByBarcode(@Param() { barcode }: ProductBarcodeDto): Promise<ProductDto> {
+  getProductByBarcode(@Param() { barcode }: ProductBarcodeDto): Promise<ProductShortDto> {
     return this.productService.getProductByBarcode(barcode, true);
   }
 
